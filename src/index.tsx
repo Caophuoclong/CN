baef0a53bb4cd18fe67bc571f8189c57342162fc
phuoclong <caophuoclong1@gmail.com>
2022-08-14 09:49:40 +0700

    feat: add chakra-ui, progressive web app

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-
+import {ChakraProvider} from '@chakra-ui/react';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <ChakraProvider>
     <App />
-  </React.StrictMode>
+  </ChakraProvider>
 );
 
 // If you want your app to work offline and load faster, you can change
